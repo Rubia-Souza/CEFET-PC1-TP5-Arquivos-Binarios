bed8ee55968f6723d3cc7b79bc61feedda89161a
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-01-29 13:30:32 -0300

    feat(utils) Adiciona funções para obter informações do caminho do arquivo

--- src/utils/fileUtils.c
+++ src/utils/fileUtils.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+
+#include ".\fileUtils.h"
 
 void createFolder(const char path[]) {
     struct stat directory = {0};
@@ -19,4 +22,48 @@ void createFile(const char path[]) {
     fclose(fileRef);
 
     return;
+}
+
+char* getFolderPath(const char filePath[_MAX_PATH]) {
+    char copyFilePath[_MAX_PATH];
+    strcpy(copyFilePath, filePath);
+
+    const size_t filePathSize = strlen(filePath);
+    if(filePathSize == 0) {
+        return NULL;
+    }
+
+    char* path = (char*) malloc(_MAX_PATH * sizeof(char));
+
+    int finalIndex = 0;
+    for(finalIndex = filePathSize-1; filePath[finalIndex] != '\\' && finalIndex >= 0; finalIndex--);
+    if(finalIndex < 0 || finalIndex+1 >= filePathSize) {
+        return NULL;
+    }
+
+    strncpy(path, copyFilePath, finalIndex+1);
+    path[finalIndex+1] = '\000';
+    return path;
+}
+
+char* getFileNameFromPath(const char filePath[_MAX_PATH]) {
+    char copyFilePath[_MAX_PATH];
+    strcpy(copyFilePath, filePath);
+
+    const size_t filePathSize = strlen(filePath);
+    if(filePathSize == 0) {
+        return NULL;
+    }
+
+    char* fileName = (char*) malloc(_MAX_PATH * sizeof(char));
+
+    int startIndex = 0;
+    for(startIndex = filePathSize-1; filePath[startIndex] != '\\' && startIndex >= 0; startIndex--);
+    if(startIndex < 0 || startIndex+1 >= filePathSize) {
+        return NULL;
+    }
+
+    strncpy(fileName, &copyFilePath[startIndex+1], filePathSize - startIndex);
+    fileName[filePathSize - startIndex+1] = '\000';
+    return fileName;
 }
--- src/utils/fileUtils.h
+++ src/utils/fileUtils.h
@@ -1,7 +1,11 @@
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
+#include <stdlib.h>
+
 void createFolder(const char path[]);
 void createFile(const char path[]);
+char* getFolderPath(const char fileName[_MAX_PATH]);
+char* getFileNameFromPath(const char filePath[_MAX_PATH]);
 
 #endif
